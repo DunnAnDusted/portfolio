d43dae8eda672e4117e568a9f796ac0ac8ddf4c4
Andrew Dunn <91694574+DunnNDusted@users.noreply.github.com>
2021-12-24 03:02:11 +0000

    Tweak implementation + add comments (#7)
    * refactor(cli_commands): Tweak implementation + add comments
    
    This will tweak the implementations of both CLI commands to clean them up slightly, as well as add documentation for them, commenting how these processes work.
    
    BREAKING CHANGE: Note: While the `lib.rs` module for the `minigrep` binary exists in a very limited scope, the tweaks to the naming of the public interface of the `Config` struct, would still constitute a breaking change in the case they'd seen implementation.

--- cli_commands/src/bin/minigrep/main.rs
+++ cli_commands/src/bin/minigrep/main.rs
@@ -6,13 +6,13 @@ use std::{
 };
 
 fn main() {
-    let config = lib::Config::new(env::args())
+    let config = lib::Config::new(env::args().skip(1)) // Attempts to construct a new minigrep config struct, based on the command arguments minus the first file path argument.
         .unwrap_or_else(|err| {
-            eprintln!("{}", err);
-            process::exit(1); 
+            eprintln!("usage: minigrep <Text: RegEx> <Text: File Path>\n\narguments cannot be parsed: {}", err);
+            process::exit(1); // Prints usage and error, then exits the process, if a `Config` struct can't be constructed.
         });
 
     if let Err(err) = lib::run(config) {
-        eprintln!("File reading error: {}", err);
+        eprintln!("file reading error: {}", err); // Runs the main process of the command, and prints and error if the specified file can't be found.
     }
 }
