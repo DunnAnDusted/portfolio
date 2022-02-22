b0b8f0e9213d73f3e731d6362c3affc455d232a4
Andrew Dunn <andrew.dunn@ajja.org.uk>
2022-02-22 19:59:30 +0000

    test(tests): Clean-up tests

--- my_rusttools/tests/gcacher_tests.rs
+++ my_rusttools/tests/gcacher_tests.rs
@@ -1,18 +1,14 @@
-#![allow(unused_assignments, unused_variables)]
-use std::collections::HashMap;
-
 use my_rusttools::GCacher;
     
 #[test]
 fn it_works() {
     let mut cache = GCacher::new(|x: &usize|x + 1);
 
-    assert_eq!(&2, cache.value_from(1));
-    assert_eq!(&2, cache.value_from(1));
+    assert_eq!(2, *cache.value_from(1));
 }
 
 #[test]
-fn get_value_test() {
+fn immutable_lock() {
     let mut cache = GCacher::new(|x: &usize|x * x);
     cache.value_from(2);
 
@@ -22,50 +18,49 @@ fn get_value_test() {
 }
 
 #[test]
-fn deref_test() {
-    let cache = GCacher::new(|x: &usize|x * x);
-    let __test = &*cache;
-}
-
-#[test]
-fn capture_test() {
+fn instancer_capturing() {
     let mut num = 0;
     num += 1;
     let mut cache = GCacher::new(|x: &usize|x + num);
 
     cache.value_from(2);
-    num += 1;
-}
-
-#[test]
-fn type_anno_test() {
-    let mut test = GCacher::new(|x: &&str|x.bytes());
-    test.value_from(&"Test".to_string());
-}
-
-#[test]
-fn deconstruction_test() {
-    let mut num = 0;
-    num += 1;
-    let cache = GCacher::new(|x: &usize|x + num);
-
-    let instancer = cache.instancer;
     // Invalid incriment. Causes an error.
     // num += 1;
-    let cache = GCacher::new(instancer);
-    drop(cache);
+    cache.value_from(3);
     num += 1;
-    let mut cache = GCacher::new(|x: &String|x.to_lowercase());
 
-    let instancer = cache.instancer;
-    println!("{}", instancer(&"Test".to_string()));
-    println!("{}", cache.value_from("Test".to_string()));
+    assert_eq!(num, 2);
 }
 
 #[test]
-fn into_test() {
+fn gcacher_instancer_deconstruction() {
     let cache = GCacher::new(|x: &usize|x * x);
-    let test: HashMap<_, _> = cache.into();
-    let cache = GCacher::new(|x: &usize|x * x);
-    let (t1, t2) = cache.into();
+    let instancer = cache.into_instancer();
+
+    let mut cache = GCacher::new(instancer);
+
+    let instancer = cache.instancer();
+    assert_eq!(4, instancer(&2));
+    assert_eq!(16, *cache.value_from(4));
+    // `cache` mutation invalidates future calls of `instancer`.
+    // println!("{}", instancer(&String::from("Test")));
+}
+
+#[test]
+fn gcacher_cache_deconstruction() {
+    let mut cache = GCacher::new(|x: &usize|x * x);
+    cache.value_from(2);
+
+    let cache = cache.into_cache();
+    assert_eq!(cache.get(&2), Some(&4));
+}
+
+#[test]
+fn gcacher_inner_deconstruction() {
+    let mut cache = GCacher::new(|x:& usize|x * x);
+    cache.value_from(2);
+
+    let (instancer, cache) = cache.into_inner();
+    assert_eq!(cache.get(&2), Some(&4));
+    assert_eq!(instancer(&2), 4);
 }
