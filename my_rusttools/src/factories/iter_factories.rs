910c42d911b5642487512db3dfa68961f5bc1028
Andrew Dunn <andrew.dunn@ajja.org.uk>
2022-02-28 03:45:51 +0000

    refactor(factories::iter_factories): `successors()` + Type aliases refactor
    Previously, the `sieve_primes()` was implemented using `RangeFrom`, `skip()` and `step_by()` iterator adapter, to iterate over the indexes for primes. Due to utilising the `advance_by()` method to eagerly advance an iterator, this would potentially result in a lot of unnecessary instructions, where a simple mathematical sequence can instead be used.
    
    As such, this changes the implementation to instead use the `successors()` function to create these sequences, including using a `fold()` method over the multiples of each index and mark them as not primes. This is alongside a guard clause for the closure for the odd indexes fold, to check the state of the indexes before iterating over the multiple of the index, avoiding doing so if unnecessary.
    
    Additionally, the return types of the functions in the module tended to be `impl Iterator`, which had the unfortunate issue of not resulting in concrete return types, despite having consistant return types. Due to this unfortunate implementation fact, I'd always been unhappy, due to this resulting in only being able to use the implementation tied to the `Iterator` trait, without listing every trait the return type used at length, but I struggled to exchange this implementaiton for type aliases, due to lacking the knowledge to correctly structure and apply them. This finally applies this change, due to working out how to correctly structure aliases and their returns.
    
    Finally, this also adds a test to check primes up to 10000.

--- my_rusttools/src/factories/iter_factories.rs
+++ my_rusttools/src/factories/iter_factories.rs
@@ -1,12 +1,132 @@
 use std::{
-    ops::RangeBounds,
-    iter,
+    ops::{RangeBounds, RangeFrom},
+    iter::{
+        self, 
+        FilterMap,
+        Enumerate,
+        Map,
+        Zip,
+        Cycle,
+        Chain,
+        Take,
+        Repeat,
+        Once,
+        RepeatWith,
+        OnceWith,
+        FlatMap
+    }, 
+    vec::IntoIter,
 };
+
+/// A specialised iterator type for returning prime numbers.
+/// 
+/// This typedef is used to give the return of [`sieve_primes`],
+/// a concrete return type, allowing the usage of methods defined on the aliased type,
+/// without needing to list every trait in the method signiture.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use my_rusttools::factories::sieve_primes;
+/// 
+/// let primes = sieve_primes(10);
+/// 
+/// assert!(primes.eq(vec![2, 3, 5, 7]));
+/// ```
+pub type SievePrimes<F> = FilterMap<Enumerate<IntoIter<bool>>, F>;
+
+/// A specialised iterator type for returning the **FizzBuzz** sequence.
+/// 
+/// This typedef is used to give the return of [`fizzbuzz`],
+/// a concrete return type, allowing the usage of methods defined on the aliased type,
+/// without needing to list every trait in the method signiture.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use my_rusttools::factories::fizzbuzz;
+/// 
+/// assert_eq!(Some("FizzBuzz".to_string()), fizzbuzz().nth(14));
+/// ```
+pub type FizzBuzz<F, I, J> = Map<Zip<RangeFrom<usize>, Zip<I, J>>, F>;
+
+/// A specialised iterator type for cycling a distinct value into a sequence,
+/// at a regular interval.
+/// 
+/// This typedef is used to give the return of [`repeat_interval`],
+/// a concrete return type, allowing the usage of methods defined on the aliased type,
+/// without needing to list every trait in the method signiture.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use my_rusttools::factories::repeat_interval;
+/// 
+/// let mut fizzy = repeat_interval("Fizz", 3);
+/// 
+/// // First two values are empty string slices.
+/// assert_eq!(fizzy.next(), Some(""));
+/// assert_eq!(fizzy.next(), Some(""));
+/// 
+/// // The third value is now `"Fizz"`.
+/// assert_eq!(fizzy.next(), Some("Fizz"));
+/// 
+/// // The fourth and fifth values are empty string slices again.
+/// assert_eq!(fizzy.next(), Some(""));
+/// assert_eq!(fizzy.next(), Some(""));
+/// ```
+pub type RepeatInterval<T> = Cycle<Chain<Take<Repeat<T>>, Once<T>>>;
+
+/// A specialised iterator type for cycling the result of a closure into a sequence,
+/// at a regular interval.
+/// 
+/// This typedef is used to give the return of [`repeat_interval_with`],
+/// a concrete return type, allowing the usage of methods defined on the aliased type,
+/// without needing to list every trait in the method signiture.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use my_rusttools::factories::repeat_interval_with;
+/// 
+/// let mut fizzy = repeat_interval_with(||String::from("Fizz"), 3);
+/// 
+/// // First two values are empty strings.
+/// assert_eq!(fizzy.next(), Some("".to_owned()));
+/// assert_eq!(fizzy.next(), Some("".to_owned()));
+/// 
+/// // The third value is now `"A"`.
+/// assert_eq!(fizzy.next(), Some("Fizz".to_owned()));
+/// 
+/// // The fourth and fifth values are empty strings again.
+/// assert_eq!(fizzy.next(), Some("".to_owned()));
+/// assert_eq!(fizzy.next(), Some("".to_owned()));
+/// ```
+pub type RepeatIntervalWith<D, F> = Cycle<Chain<Take<RepeatWith<D>>, OnceWith<F>>>;
+
+/// A specialised iterator type for repeating provided value,
+/// a specified number of times.
+/// 
+/// This typedef is used to give the return of [`repeat_values`],
+/// a concrete return type, allowing the usage of methods defined on the aliased type,
+/// without needing to list every trait in the method signiture.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use my_rusttools::factories::repeat_values;
+/// 
+/// let foobar_1 = repeat_values(&[("Foo", 2), ("Bar", 3)]);
+/// let foobar_2 = ["Foo", "Foo", "Bar", "Bar", "Bar"];
+/// 
+/// asser
+pub type RepeatValues<T, F> = FlatMap<IntoIter<(T, usize)>, Take<Repeat<T>>, F>;
 
 /// Creates an iterator which returns all the primes,
 /// less than or equal to `upper_bound`.
 /// 
 /// # Examples
+/// 
 /// ```
 /// # use my_rusttools::factories::sieve_primes;
 /// #
@@ -14,19 +134,23 @@ use std::{
 /// 
 /// assert!(primes.eq(vec![2, 3, 5, 7]));
 /// ```
-pub fn sieve_primes(upper_bound: usize) -> impl Iterator<Item = usize> {
-    match upper_bound {
-        0 | 1 => Vec::new(), // Escapes the 0 and 1 cases early.
-        x => (3usize..).step_by(2) // Only steps over odd values, even value inherrantly not being prime.
-            .take_while(|i|i * i <= x)
-            .fold(vec![true; x + 1],|mut acc, i|{
-                acc.iter_mut()
-                    .skip(i * i) // The given index may be a prime number, so is skipped.
-                    .step_by(i) // Steps through the multiple of the index.
-                    .for_each(|x|*x = false); // Marking each multiple as not prime.
-                acc // Returns `Vec` of prime markers.
-            }),
-    }.into_iter()
+pub fn sieve_primes(upper_bound: usize) -> SievePrimes<impl FnMut((usize, bool)) -> Option<usize>> {
+    iter::successors(Some(3usize), |x|x.checked_add(2)) // Only steps over odd values, even value inherrantly not being prime.
+        .take_while(|i|i * i <= upper_bound)
+        .fold(vec![true; upper_bound + 1], |acc, i|{
+            if !acc[i] {
+                return acc; // Guard returns early if for indexes which have already been unmarked as prime.
+            }
+
+            // Starting with the first multiple of the index, due to the index potentially being prime,
+            // it's added to the previous value with each iteration, to get the next.
+            iter::successors(Some(i * i), |x|x.checked_add(i))
+                .take_while(|&i|i <= upper_bound) 
+                .fold(acc, |mut acc, y|{
+                    acc[y] = false; // Marks the iteration index as not prime.
+                    acc
+                })
+        }).into_iter()
         .enumerate()
         .filter_map(|x|match x {
             (0 | 1, _) => None, // Discards indexes `0` and `1`, due to being ruled against being primes.
@@ -83,13 +207,14 @@ where
 /// [`Step`]: std::iter::Step
 /// 
 /// # Examples
+/// 
 /// ```
 /// # use my_rusttools::factories::fizzbuzz;
 /// #
 /// assert_eq!(Some("FizzBuzz".to_string()), fizzbuzz().nth(14));
 /// ```
 #[inline]
-pub fn fizzbuzz() -> impl Iterator<Item = String> {
+pub fn fizzbuzz() -> FizzBuzz<impl FnMut((usize,(&'static str, &'static str))) -> String, impl Iterator<Item = &'static str>, impl Iterator<Item = &'static str>> {
     // Sets up cycling iterators, with `Fizz` and `Buzz` values at the appropriate intervals,
     // zipping them into a single iterator.
     let fizzbuzz = repeat_interval("Fizz", 3).zip(repeat_interval("Buzz", 5));
@@ -133,7 +258,7 @@ pub fn fizzbuzz() -> impl Iterator<Item = String> {
 /// assert_eq!(fizzy.next(), Some(""));
 /// ```
 #[inline]
-pub fn repeat_interval<T: Clone + Default>(repeat: T, interval: usize) -> impl Iterator<Item = T> {
+pub fn repeat_interval<T: Clone + Default>(repeat: T, interval: usize) -> RepeatInterval<T> {
     iter::repeat(Default::default())
         .take(interval - 1)
         .chain(iter::once(repeat))
@@ -169,7 +294,7 @@ pub fn repeat_interval<T: Clone + Default>(repeat: T, interval: usize) -> impl I
 /// assert_eq!(fizzy.next(), Some("".to_owned()));
 /// ```
 #[inline]
-pub fn repeat_interval_with<T: Default, F: Clone>(repeat: F, interval: usize) -> impl Iterator<Item = T> where
+pub fn repeat_interval_with<T: Default, F: Clone>(repeat: F, interval: usize) -> RepeatIntervalWith<impl FnMut() -> T + Clone, F> where
 F: FnMut() -> T, {
     iter::repeat_with(Default::default)
         .take(interval - 1)
@@ -190,7 +315,7 @@ F: FnMut() -> T, {
 /// 
 /// assert!(foobar_1.eq(foobar_2));
 /// ```
-pub fn repeat_values<T: Clone>(repeat: &[(T, usize)]) -> impl Iterator<Item = T> {
+pub fn repeat_values<T: Clone>(repeat: &[(T, usize)]) -> RepeatValues<T, impl FnMut((T, usize)) -> Take<Repeat<T>>> {
     repeat.to_owned()
         .into_iter()
         .flat_map(|(x, y)|iter::repeat(x).take(y))
