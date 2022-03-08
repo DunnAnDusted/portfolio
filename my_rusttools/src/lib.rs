56b65c92440391b83af0e26711a4109eb8caaa3e
Andrew Dunn <andrew.dunn@ajja.org.uk>
2022-03-08 02:31:07 +0000

    feat(pigify): Add pre-allocation + Misc tests
    Previously, the `pigify()` function would simply allocate an empty string to append the resulting Pig Latin `String` to.
    
    Given the resulting string should scale somewhat consistently with the number of words in the string plus it's length, this meant an expected capacity could be calculated. As such, this updates the implementation to include that calculation and resulting allocation, alongside changing the return type to allow for overflow errors, as a formality.
    
    Additionally, this creates a new file for misc. integrsation tests, notably adding a test for `pigify()` against the quick brown fox phrase.
    
    BREAKING CHANGE: Due to the including for cases of overflow errors, this changes the signiture for the `pigify()` command.

--- my_rusttools/src/lib.rs
+++ my_rusttools/src/lib.rs
@@ -11,20 +11,35 @@ use unicode_segmentation::UnicodeSegmentation;
 /// Roughly translates the provided string
 /// into Pig Latin!
 /// 
+/// # Errors
+/// 
+/// Will return an `Err` varient, if the expected size of the resulting string exceeds `usize::MAX`.
+/// Note: This calculation is somewhat rough, meaning the actual resulting length may have been less than calculated,
+/// but the process is more to allow pre-allocation of required memory,
+/// rather than handling cases where string capacity would be exceeded.
+/// As such, the error case is more of a formality of handling the calculation results.
+/// 
 /// # Example
 /// 
 /// ```
 /// use my_rusttools::pigify;
 /// 
 /// let pigified = pigify("Example");
-/// assert_eq!("Example-hay", pigified.as_str());
+/// assert_eq!(Ok("Example-hay".to_string()), pigified);
 /// ```
-pub fn pigify(convert: &str) -> String {
+pub fn pigify(convert: &str) -> Result<String, &'static str> {
     static VOWELS: &[char] = &['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
 
-    convert.trim()
+    // Calculates rough length for the resulting string.
+    let cap = convert.unicode_words()
+        .count()
+        .checked_mul(4)
+        .and_then(|x|x.checked_add(convert.len()))
+        .ok_or("to long to convert")?;
+
+    let mut ret = convert.trim()
         .split_word_bounds()
-        .fold(String::new(), |acc, x| {
+        .fold(String::with_capacity(cap), |acc, x| {
             // Guard for cases where the item isn't a word.
             if !x.contains(char::is_alphabetic) {
                 return acc + x;
@@ -39,5 +54,8 @@ pub fn pigify(convert: &str) -> String {
             // Reformats the values as a new string, trimming leading cases,
             // before being appended to the builder string and returning it.
             acc + format!("{}{}-{}ay", header_graph, curr_graphs.as_str(), ay_graph).trim_start_matches('-')
-        })
+        });
+
+        ret.shrink_to_fit();
+        Ok(ret)
 }
---
+++ my_rusttools/tests/misc_tests.rs
@@ -0,0 +1,9 @@
+use my_rusttools::pigify;
+
+#[test]
+fn tqbf_pigified() {
+    let tqbf = "the quick brown fox jumped over the lazy dog";
+    let tqbf_pigified = "he-tay uick-qay rown-bay ox-fay umped-jay over-hay he-tay azy-lay og-day";
+
+    assert_eq!(Ok(tqbf_pigified.to_owned()), pigify(tqbf));
+}
