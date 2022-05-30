bd326432359d3d58f15c32002b0cdde15ea551df
Andrew Dunn <andrew.dunn@ajja.org.uk>
2022-05-30 02:01:54 +0100

    refactor(my_rusttools::examples::guessing_game): Improve error handling
    Previously, my `guessing_game.rs` example, implicitly utilised exiting from the process upon encountering an IO error, using the `ParseStdinExtended` struct. This commit, improves the implementation of the example, by propagating errors out of the `main` function, rather than calling an exit function.
    
    Additionally, providing an out of range value, is now explicitly highligted as an error case, via `match` case validation.

--- my_rusttools/examples/guessing_game/main.rs
+++ my_rusttools/examples/guessing_game/main.rs
@@ -1,30 +1,37 @@
-use std::cmp::Ordering;
+use std::{io, cmp::Ordering};
 use rand::Rng;
-use my_rusttools::ParseStdinExtended;
+use my_rusttools::StdinExtended;
 
-fn main() {
+fn main() -> io::Result<()> {
     let secret: u8 = rand::thread_rng().gen_range(1..=100);
-    let cli_inp = ParseStdinExtended::new();
+    let cli_inp = StdinExtended::new();
     println!("Guess the number!");
 
     loop {
         let guess: u8 = loop {
-            if let x @ 0..=100 = cli_inp.read_line_until_parsed(
-                ||println!("Please enter a number from 1 to 100,"),
-                |err|eprintln!("invalid input: {err}")
-            ) {
-                break x;
+            println!("Please enter a number from 1 to 100:");
+            
+            let guess = cli_inp.read_line_new_string()?
+                .trim()
+                .parse();
+
+            print!("\x1B[2J\x1B[1;1H");
+
+            match guess {
+                Ok(x @ 1..=100) => break x,
+                Ok(x) => println!("invalid input: guess {x}, doesn't fall within range 1..100"),
+                Err(err) => println!("invalid input: {err}")
             }
         };
 
-        println!("Your guess: {}", guess);
+        println!("Your guess: {guess}");
 
         match guess.cmp(&secret) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too large!"),
             Ordering::Equal => {
                 println!("You win!");
-                break;
+                return Ok(());
             }
         }
     }
