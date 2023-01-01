905dad8b03693d7263d6725a6511daf76d61636c
Andy <andrew.dunn@ajja.org.uk>
2023-01-01 18:52:59 +0000

    fix(my_rusttools): Fix + improve pigify impl
    Turned out I broke `my_rusttools::pigify`, so alongside fixing it,
    this commit also removed the unnecessary use of `Iterator::filter`,
    relocates the `VOWELS` constant, and switches to using `Option::map_or`,
    to unwrap the processed string or return the unprocessed one.

--- my_rusttools/src/lib.rs
+++ my_rusttools/src/lib.rs
@@ -10,58 +10,55 @@ use unicode_segmentation::UnicodeSegmentation;
 
 /// Roughly translates the provided `&str`
 /// into Pig Latin!
-/// 
+///
 /// # Panics
-/// 
+///
 /// May panic if `convert` contains a byte sequence
 /// which would fail to produce a valid grapheme cluster.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// use my_rusttools::pigify;
-/// 
+///
 /// let pigified = pigify("Example");
 /// assert_eq!(pigified, "Example-hay");
 /// ```
 pub fn pigify(convert: &str) -> String {
-    use std::borrow::Cow::*;
-
-    const VOWELS: &[char] = &['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+    use std::borrow::Cow;
 
     convert.trim()
         .split_word_bounds()
-        // Empty strings make no sense, but could cause errors...
-        .filter(|x| !x.is_empty())
         // Checks whether an item should be processed (contains Latin characters).
-        .map(|x| x.starts_with(|y| matches!(y, 'a'..='z' | 'A'..='Z'))
-            .then(|| {
-                let mut curr_graphs = x.graphemes(true);
+        .map(|x| {
+            x.starts_with(|y| matches!(y, 'a'..='z' | 'A'..='Z'))
+                .then(|| {
+                    const VOWELS: &[char] = &['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
 
-                // TODO: `None` varient means something funky happened with the string,
-                // and I'm not sure how I'd deal with that currently,
-                // other than leaving it to panic...
-                let head = curr_graphs.next()
-                    .unwrap_or_else(|| panic!("invalid `&str`: {x}"));
+                    let mut graphemes = x.graphemes(true);
 
-                // Removes the first grapheme from the word,
-                // to be `ay_head` if it doesn't contain a vowel.
-                let (ret, ay_head) = head.contains(VOWELS)
-                    .then_some((head, "h"))
-                    .unwrap_or_else(|| (curr_graphs.as_str(), head));
+                    let head = graphemes.next()
+                        .expect(
+                            "this can only be caused due to an empty string, which shouldn't be possible, \
+                            because empty strings don't start with *anything*"
+                        );
 
-                let mut ret = ret.to_owned();
+                    // If the first grapheme is a vowel, it should remain the head of the word.
+                    // If it's instead a consenant, it should be moved to the back of the string.
+                    let (ret, ay_head) = head.contains(VOWELS)
+                        .then_some((head, "h"))
+                        .unwrap_or(("", head));
+                    let mut ret = ret.to_owned() + graphemes.as_str();
 
-                // Preceeding hyphen looks weird,
-                // so should only be pushed if it isn't the first character.
-                if !ret.is_empty() {
-                    ret.push('-');
-                }
+                    // Only push hyphen to returned string, if the string isn't empty,
+                    // because preceeding hyphen doesn't look right...
+                    if !ret.is_empty() {
+                        ret.push('-');
+                    }
 
-                // Returns a `Cow`, due to only conditionally allocating memory.
-                Owned(ret + ay_head + "ay")
-            })
-            .unwrap_or(Borrowed(x))
-        )
+                    ret + ay_head + "ay"
+                })
+                .map_or(Cow::Borrowed(x), Cow::Owned)
+        })
         .collect()
 }
